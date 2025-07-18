6c50babe83a7451617a52cb3946515df2d7de7d4
Doffannoel <noelsihotang2004@gmail.com>
2025-07-18 19:56:23 +0700

    fix(talent-growth): fix incorrect app path in settings and apps.py

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taskmanager.tasks',
+    'tasks',
     'rest_framework',
     'django_filters',
     'drf_yasg',
